49792e3e87452db36e549a97492ae0f7e41b0a5b
tanmaylohakare <tanmaylohakare820@gmail.com>
2024-11-27 11:37:56 +0530

    Check Employee is Present Or Absent

--- src/com/tech/Employee.java
+++ src/com/tech/Employee.java
@@ -4,6 +4,17 @@ public class Employee {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee Wage Computation ");
 		
+		 int IS_PRESENT=1;
+	        int attendence=(int) (Math.random()*2);
+	        if(attendence== IS_PRESENT)
+	        {
+	            System.out.println("Employee is Present ");
+	        }
+	        else
+	        {
+	            System.out.println("Employee is Absent");
+	        }
+		
 	}
 
 }
