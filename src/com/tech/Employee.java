0763c7510ade1b367b376634637079da2df76503
tanmaylohakare <tanmaylohakare820@gmail.com>
2024-11-27 14:16:04 +0530

    Added the Calculation Of Monthly Wage

--- src/com/tech/Employee.java
+++ src/com/tech/Employee.java
@@ -7,7 +7,19 @@ public class Employee {
 		final int Full_Time=1;
 		final int Part_Time=2;
 		final int Wage_Per_Hr=20;
+		final int working_days_per_mounth=20;
+		final int max_working_Hrs=100;
 		
+		
+		int totalWagesForHrs =0;
+		int totalWagesForDays =0;
+		int totalWagesForMonth =0;
+		
+		
+//		int workingHrs=0;
+		
+		for(int day=1; day<=working_days_per_mounth;day++) {
+			
 		int workingHrs=0;
         int empType=(int) (Math.random()*100)%3;
 
@@ -38,8 +50,11 @@ public class Employee {
 	            
 		 int wage=workingHrs *Wage_Per_Hr;
 		 
-		 System.out.println("Employee Daily wage is " + wage);
-		
+		 totalWagesForMonth +=wage;
+		 
+		 System.out.println("Day " +day +": Employee Type =" +empType + ", Daily Wage ="+ wage);
+		}
+		System.out.println("Total Wage For the Month "+ totalWagesForMonth);
 	}
 
 }
